00d1280415f99a4f98c37f737ca501d3b4b7e94f
kellyylim3004-oss <kellyylim3004@gmail.com>
2026-05-03 06:43:30 +0700

    Add multiple features and improve styling

---
+++ Pages/CoffeeExperience.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KopiCinta.Pages
+{
+    public class CoffeeExperienceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- obj/Debug/net8.0/KopiCinta.AssemblyInfo.cs
+++ obj/Debug/net8.0/KopiCinta.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KopiCinta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+422e1b7f9aa5a15d956e5f581febe5c4f9f54fe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("KopiCinta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KopiCinta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
