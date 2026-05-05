c57469791718d59f4fe4f15587a64ad47986cc51
kellyylim3004-oss <kellyylim3004@gmail.com>
2026-05-05 18:02:02 +0700

    fixed coffee

--- obj/Debug/net8.0/KopiCinta.AssemblyInfo.cs
+++ obj/Debug/net8.0/KopiCinta.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KopiCinta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18203c3ee46f13035cf192f167557152aa312fbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d61a434a1eea3e2f48257c7339fd89d865d140ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("KopiCinta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KopiCinta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
