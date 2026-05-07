881f54de049c580e291e1b28bb5dbe1882f3666d
kellyylim3004-oss <kellyylim3004@gmail.com>
2026-05-08 00:32:34 +0700

    FINAL REVISION ON HOME

--- obj/Debug/net8.0/KopiCinta.AssemblyInfo.cs
+++ obj/Debug/net8.0/KopiCinta.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KopiCinta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaca1deb8475bc9a262a184989b07efd8da24b60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c1d4aa13266f2b711e9968f3ed7793ea861933")]
 [assembly: System.Reflection.AssemblyProductAttribute("KopiCinta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KopiCinta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
